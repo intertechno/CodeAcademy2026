c313c45ad27b4e6ff56cd634fe9400eefaae8b14
Jani Järvinen <jani.jarvinen@intertechno.org>
2026-09-08 13:18:33 +0300

    Added a C#/.NET unit testing demo for simple console application and a calculations class.

---
+++ Testing automation/DotNetUnitTestDemo/DotNetUnitTestDemo.Tests/CalculationTests.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNetUnitTestDemo.Tests
+{
+    [TestClass]
+    public sealed class CalculationTests
+    {
+        [TestMethod]
+        public void SumCalculation()
+        {
+            // arrange
+            int a = 5;
+            int b = 6;
+            int expected = a + b;
+
+            // act
+            int result = Calculations.Sum(a, b);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Testing automation/DotNetUnitTestDemo/DotNetUnitTestDemo/Calculations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetUnitTestDemo
+{
+    public static class Calculations
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
